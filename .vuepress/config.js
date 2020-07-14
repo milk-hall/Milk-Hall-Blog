59285263939bb260bd56df193736d218e34c7ea3
wucaixin <297912123@qq.com>
2020-07-15 00:00:53 +0800

    feat: 文章迁移

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,11 @@ module.exports = {
   "title": "Milk Hall",
   "description": "blog",
   "dest": "public",
+  'locales': {
+    '/': {
+      'lang': 'zh-CN'
+    }
+  },
   "head": [
     [
       "link",
@@ -53,14 +58,14 @@ module.exports = {
         ]
       }
     ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
-    },
+    // "sidebar": {
+    //   "/docs/theme-reco/": [
+    //     "",
+    //     "theme",
+    //     "plugin",
+    //     "api"
+    //   ]
+    // },
     "type": "blog",
     "blogConfig": {
       "category": {
@@ -86,11 +91,45 @@ module.exports = {
     "lastUpdated": "Last Updated",
     "author": "xg",
     "authorAvatar": "/avatar.png",
-    "record": "xxxx",
+    "record": "",
     "startYear": "2020"
   },
   "markdown": {
     "lineNumbers": true
   },
-  plugins: ["@vuepress-reco/vuepress-plugin-kan-ban-niang"]
+  plugins: [
+    [
+      "@vuepress-reco/vuepress-plugin-kan-ban-niang"
+    ],
+    [
+      "@vuepress-reco/vuepress-plugin-bgm-player",
+      // {
+      //   audios: [
+      //     // 本地文件示例
+      //     {
+      //       name: '장가갈 수 있을까',
+      //       artist: '咖啡少年',
+      //       url: '/bgm/1.mp3',
+      //       cover: '/bgm/1.jpg'
+      //     },
+      //     // 网络文件示例
+      //     {
+      //       name: '강남역 4번 출구',
+      //       artist: 'Plastic / Fallin` Dild',
+      //       url: 'https://assets.smallsunnyfox.com/music/2.mp3',
+      //       cover: 'http://p1.music.126.net/M6I9g1EkDxbgl2j5Ygi-lQ==/130841883720226.jpg?param=130y130'
+      //     },
+      //     {
+      //       name: '用胳膊当枕头',
+      //       artist: '최낙타',
+      //       url: 'https://assets.smallsunnyfox.com/music/3.mp3',
+      //       cover: 'https://assets.smallsunnyfox.com/music/3.jpg'
+      //     }
+      //   ]
+      // }
+    ],
+    [
+      "vuepress-plugin-auto-sidebar",
+    ]
+  ],
 }
